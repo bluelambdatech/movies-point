ec4bcbf0e0f317fdae369cc7ed02d4b49abb26c8
Blue Lambda Technologies LLC <114599058+bluelambdatech@users.noreply.github.com>
2023-10-15 16:30:54 -0400

    Update bash_script.sh

--- utils/bash_script.sh
+++ utils/bash_script.sh
@@ -5,21 +5,20 @@
 
 sudo apt update
 sudo apt upgrade -y
- mkdir movies
-sudo apt install python
-python install pip
-sudo apt install pip
-sudo apt install python-pip
-sudo pip install virtualenv
-mkdir -p movies/source
- cd movies
-virtualenv env
+sudo rm -rf movies
+sudo mkdir ~/movies
+cd ~/movies
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo python3 -m venv venv
 sudo apt install git -y
-git clone https://github.com/bluelambdatech/movies-point.git
-cd movies-point/
-source env/bin/activate
-git checkout dev
-sudo pip install -r requirements.txt 
-uvicorn main:app --host 0.0.0.0 --port 80
+sudo git clone https://github.com/bluelambdatech/movies-point.git
+cd movies-point
+sudo git checkout dev
+sudo cp /project/movies-point/.env ~/movies/movies-point
+sudo virtualenv venv
+source venv/bin/activate
+sudo pip3 install -r requirements.txt
+sudo uvicorn main:app --host 0.0.0.0 --port 80
 
 
